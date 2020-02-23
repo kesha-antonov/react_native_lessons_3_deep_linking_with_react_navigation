f72521399f10e5a13b2c01da2bf4e1ab23f68a9d
Kesha Antonov <innokenty.longway@gmail.com>
2020-02-23 12:42:12 +0800

    initial

---
+++ android/app/src/main/java/com/react_native_lessons_3_deep_linking_with_react_navigation/MainActivity.java
@@ -0,0 +1,15 @@
+package com.react_native_lessons_3_deep_linking_with_react_navigation;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "react_native_lessons_3_deep_linking_with_react_navigation";
+  }
+}
